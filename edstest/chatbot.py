960f8ea7235479bde8ab8d71c42a3a2c6f14d9bf
caitlinhornigold1 <caitlin.hornigold@gmail.com>
2026-09-17 19:14:26 +1200

    llama test

---
+++ edstest/chatbot.py
@@ -0,0 +1,19 @@
+import ollama
+
+while True:
+    question = input("You: ")
+
+    if question.lower() == "exit":
+        break
+
+    response = ollama.chat(
+        model="llama3.2",
+        messages=[
+            {
+                "role": "user",
+                "content": question
+            }
+        ]
+    )
+
+    print("Llama:", response["message"]["content"])
